21847ad7034ca95081a75c51399d540fe553524b
Shubham17kr <shubhamkumar97423@gmail.com>
2023-02-21 21:26:26 +0530

    generate unique id

--- module3/jira/script.js
+++ module3/jira/script.js
@@ -6,7 +6,10 @@ let allColorcont=document.querySelectorAll(".priority-color")
 let rmvbtn=document.querySelector(".remove-btn")
 let removeflag=false;
 let flag=true;
-let modalpriorityColor="black"
+let color=["lightpink","lightblue","lightgreen","black"]
+let modalpriorityColor=color[color.length-1]
+
+var uid = new ShortUniqueId();
 
 addbtn.addEventListener("click",function(){
 
@@ -67,13 +70,58 @@ function createTicket(ticketColor,task)
     let ticketCont=document.createElement('div')
     ticketCont.setAttribute('class','ticket-cont')
     ticketCont.innerHTML=`<div class="ticket-color ${ticketColor}"></div>
-                          <div class="ticket-id">#er456</div>
-                          <div class="ticket-area">${task}</div>`
+                          <div class="ticket-id">#${uid()}</div>
+                          <div class="ticket-area">${task}</div>
+                          <div class="lock-unlock"><i class="fa fa-lock"></i></div>`
     mainCont.append(ticketCont)
+
+    // lock-unlock
+
+    let lockunlockbtn=ticketCont.querySelector(".lock-unlock i")
+    let text_area=ticketCont.querySelector(".ticket-area")
+    lockunlockbtn.addEventListener("click",function(){
+        if(lockunlockbtn.classList.contains("fa-lock"))
+        {
+            lockunlockbtn.classList.remove("fa-lock")
+            lockunlockbtn.classList.add("fa-unlock")
+            text_area.setAttribute("contentEditable","true")
+
+        }
+        else{
+            lockunlockbtn.classList.remove("fa-unlock")
+            lockunlockbtn.classList.add("fa-lock")
+            text_area.setAttribute("contentEditable","false")
+        }
+    })
 
     ticketCont.addEventListener("click",function(){
         if(removeflag){
             ticketCont.remove();
         }
     })
+
+    let ticketcolorBand=ticketCont.querySelector(".ticket-color")
+    ticketcolorBand.addEventListener("click",function(){
+
+        let prevticketcolor=ticketcolorBand.classList[1]
+    let prevticketcolorIndex=-1;
+
+    for(let i=0;i<color.length;i++)
+    {
+        if(prevticketcolor==color[i])
+        {
+            prevticketcolorIndex=i;
+            break;
+        }
+    }
+    let nextticketcolorIndex=(prevticketcolorIndex+1)%color.length
+    let nextticketcolor=color[nextticketcolorIndex]
+      
+    ticketcolorBand.classList.remove(prevticketcolor)
+    ticketcolorBand.classList.add(nextticketcolor) 
+    })
+    
+
+
+
 }
