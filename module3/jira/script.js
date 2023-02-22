a3f729944a82f46e9132fa49e4dfdfd64b0ae929
Shubham17kr <shubhamkumar97423@gmail.com>
2023-02-22 16:24:45 +0530

    update at ticketArr

--- module3/jira/script.js
+++ module3/jira/script.js
@@ -9,7 +9,11 @@ let flag=true;
 let color=["lightpink","lightblue","lightgreen","black"]
 let modalpriorityColor=color[color.length-1]
 
+let alltoolboxcolor=document.querySelectorAll(".color")
+
 var uid = new ShortUniqueId();
+
+let ticketArr=[];
 
 addbtn.addEventListener("click",function(){
 
@@ -23,6 +27,58 @@ addbtn.addEventListener("click",function(){
     }
     flag=!flag;
 })
+
+// filter
+
+for(let i=0;i<alltoolboxcolor.length;i++)
+{
+    alltoolboxcolor[i].addEventListener("click",function(){
+        let currentcolor=alltoolboxcolor[i].classList[1];
+        let filterArr=[]
+        for(let j=0;j<ticketArr.length;j++)
+        {
+            if(ticketArr[j].color==currentcolor)
+            {
+                filterArr.push(ticketArr[j])
+            }
+        }
+        let allticket=document.querySelectorAll(".ticket-cont")
+        for(let i=0;i<allticket.length;i++)
+        {
+            allticket[i].remove();
+        }
+
+        for(let i=0;i<filterArr.length;i++)
+        {
+            let ticket=filterArr[i];
+            let color=ticket.color;
+            let task=ticket.task;
+            let id=ticket.id;
+            createTicket(color,task,id)
+        }
+    })
+
+    alltoolboxcolor[i].addEventListener("dblclick",function(){
+
+        let allticket=document.querySelectorAll(".ticket-cont")
+        for(let i=0;i<allticket.length;i++)
+        {
+            allticket[i].remove();
+        }
+
+        for(let i=0;i<ticketArr.length;i++)
+        {
+            let ticket=ticketArr[i];
+            let color=ticket.color;
+            let task=ticket.task;
+            let id=ticket.id;
+            createTicket(color,task,id)
+        }
+    })
+}
+
+
+
 
 modal.addEventListener("keydown",function(e){
 
@@ -65,18 +121,28 @@ rmvbtn.addEventListener("click",function(){
 })
 
 
-function createTicket(ticketColor,task)
+function createTicket(ticketColor,task,ticketId)
 {
+
+    let id;
+    if(ticketId==undefined)
+    {
+        id=uid();
+    }
+    else{
+        id=ticketId;
+    }
     let ticketCont=document.createElement('div')
     ticketCont.setAttribute('class','ticket-cont')
     ticketCont.innerHTML=`<div class="ticket-color ${ticketColor}"></div>
-                          <div class="ticket-id">#${uid()}</div>
+                          <div class="ticket-id">#${id}</div>
                           <div class="ticket-area">${task}</div>
                           <div class="lock-unlock"><i class="fa fa-lock"></i></div>`
     mainCont.append(ticketCont)
 
     // lock-unlock
-
+      
+    // update UI
     let lockunlockbtn=ticketCont.querySelector(".lock-unlock i")
     let text_area=ticketCont.querySelector(".ticket-area")
     lockunlockbtn.addEventListener("click",function(){
@@ -86,20 +152,33 @@ function createTicket(ticketColor,task)
             lockunlockbtn.classList.add("fa-unlock")
             text_area.setAttribute("contentEditable","true")
 
+
         }
         else{
             lockunlockbtn.classList.remove("fa-unlock")
             lockunlockbtn.classList.add("fa-lock")
             text_area.setAttribute("contentEditable","false")
         }
+        // update ticketArr
+
+        let ticketidx=getticketIdx(id);
+        
+        ticketArr[ticketidx].task=text_area.textContent;
     })
 
     ticketCont.addEventListener("click",function(){
         if(removeflag){
+            // delete from ui
             ticketCont.remove();
+
+            //  delete from ticketArr
+
+            let ticketidx=getticketIdx(id);
+            ticketArr.splice(ticketidx,1)    // remove a ticket
         }
     })
 
+    // update UI
     let ticketcolorBand=ticketCont.querySelector(".ticket-color")
     ticketcolorBand.addEventListener("click",function(){
 
@@ -119,9 +198,28 @@ function createTicket(ticketColor,task)
       
     ticketcolorBand.classList.remove(prevticketcolor)
     ticketcolorBand.classList.add(nextticketcolor) 
+
+    // update ticketArr
+    let ticketArridx=getticketIdx(id);
+    
+    ticketArr[ticketArridx].color=nextticketcolor;
     })
     
 
+    if(ticketId==undefined){
+        ticketArr.push({"color":ticketColor,"task":task,"id":id})
+    }
+    // console.log(ticketArr)
 
-
+ 
+}
+function getticketIdx(id)
+{
+    for(let i=0;i<ticketArr.length;i++)
+    {
+        if(ticketArr[i].id==id)
+        {
+            return i;
+        }
+    }
 }
